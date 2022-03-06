65fba187a3c29ef6a58e116b22b6db8ae52dbbb5
Nivedhithya Sundarasamy <Sundarasamy.Nivedhithya@in.bosch.com>
2022-03-06 21:00:53 +0530

    TDD-Step-3: Writing the code to pass the test
    Signed-off-by: Nivedhithya Sundarasamy <Sundarasamy.Nivedhithya@in.bosch.com>

--- InterpretChargingCurrentRangeAndOccurences.c
+++ InterpretChargingCurrentRangeAndOccurences.c
@@ -2,11 +2,24 @@
 #include <stdlib.h>
 #include "InterpretChargingCurrentRangeAndOccurences.h"
 
-char* interpretChargingCurrentRangeAndOccurences(int chargingCurrentSamples[]){
-	printf("Charging current sample first element: %d \n", chargingCurrentSamples[0]);
-	int minValue=0, maxValue=0, numberOfOccurences=0;
+char* interpretChargingCurrentRangeAndOccurences(int* chargingCurrentSamples){
 	char *chargingCurrentRangeAndOccurences = (char*)malloc(100);
-
-	sprintf(chargingCurrentRangeAndOccurences, "%d-%d, %d", minValue, maxValue, numberOfOccurences);
+	int minValue, maxValue;
+	size_t numberOfOccurences;
+	numberOfOccurences = sizeof(chargingCurrentSamples) / sizeof(chargingCurrentSamples[0]);
+	minValue = 100; // Initalizing to a high value
+	maxValue = chargingCurrentSamples[0];
+	
+	for (int i=0; i<2; i++) {
+		if(chargingCurrentSamples[i] > maxValue){
+			maxValue = chargingCurrentSamples[i];
+		}
+		if(chargingCurrentSamples[i] < minValue){
+			minValue = chargingCurrentSamples[i];
+		}
+	}
+	
+	sprintf(chargingCurrentRangeAndOccurences, "%d-%d, %lu", minValue, maxValue, numberOfOccurences);
+	printf("%s\n", chargingCurrentRangeAndOccurences);
 	return chargingCurrentRangeAndOccurences;
 };
