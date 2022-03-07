b7f3d761033422a58b0ebf8daa692aa00abe4105
Nivedhithya Sundarasamy <Sundarasamy.Nivedhithya@in.bosch.com>
2022-03-07 19:58:00 +0530

    TDD-Step-4: Refactoring to bring SRP
    Signed-off-by: Nivedhithya Sundarasamy <Sundarasamy.Nivedhithya@in.bosch.com>

--- TestInterpretChargingCurrentRangeAndOccurences.cpp
+++ TestInterpretChargingCurrentRangeAndOccurences.cpp
@@ -11,9 +11,17 @@ TEST_CASE("Checks the charging current range and its occurences") {
 	REQUIRE(strcmp(rangeAndOccurences, expectedRangeAndOccurences) == 0);
 }
 
+TEST_CASE("Check the size of given charging current samples") {
+	int chargingCurrentSamples[] = {4,5};
+	size_t expectedSize = 2;
+	size_t numberOfSamples = findNumberOfSamples(chargingCurrentSamples);
+	REQUIRE(numberOfSamples == expectedSize);
+}
+
 TEST_CASE("Checks the minimum value in a given charging current range") {
 	int chargingCurrentSamples[] = {4,5};
 	int expectedMinValue = 4;
-	int minValueForChargingCurrentRange = findMinValueOfChargingCurrentRange(chargingCurrentSamples);
+	size_t numberOfSamples = findNumberOfSamples(chargingCurrentSamples);
+	int minValueForChargingCurrentRange = findMinValueOfChargingCurrentRange(chargingCurrentSamples, numberOfSamples);
 	REQUIRE(minValueForChargingCurrentRange == expectedMinValue);
 }
